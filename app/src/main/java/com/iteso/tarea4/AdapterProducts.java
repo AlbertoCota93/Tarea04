6df55b9925139bb6d03fe3f04c0afb51612b07c1
AlbertoCota93 <is694734@iteso.mx>
2018-10-01 20:47:32 -0500

    App Setup

---
+++ app/src/main/java/com/iteso/tarea4/AdapterProducts.java
@@ -0,0 +1,98 @@
+package com.iteso.tarea4;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.iteso.test.beans.ItemProduct;
+
+import java.util.ArrayList;
+
+public class AdapterProducts extends RecyclerView.Adapter<AdapterProducts.ViewHolder>{
+
+    private ArrayList<ItemProduct> products;
+    private Context context;
+
+    public AdapterProducts(Context context, ArrayList<ItemProduct> products){
+        this.context=context;
+        this.products=products;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product,parent,false);
+        ViewHolder viewHolder= new ViewHolder(v);
+        return viewHolder;
+    }
+
+    public static class  ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView image;
+        private TextView title;
+        private Button mDetail;
+        private TextView mProductStore;
+        private TextView mProductLocation;
+        private TextView mProductPhone;
+        private ImageView mProductThumbnail;
+        private RelativeLayout mEventLayout;
+
+        public ViewHolder(View v){
+            super(v);
+            image = v.findViewById(R.id.item_product_image);
+            title=v.findViewById(R.id.item_product_title);
+            mDetail=v.findViewById(R.id.item_product_detail);
+            mProductStore= v.findViewById(R.id.item_product_store);
+            mProductLocation= v.findViewById(R.id.item_product_location);
+            mProductPhone= v.findViewById(R.id.item_product_phone);
+            mProductThumbnail=v.findViewById(R.id.item_product_thumbnail);
+            mEventLayout=v.findViewById(R.id.item_product_layout);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        holder.mProductStore.setText(products.get(position).getStore());
+        holder.title.setText(products.get(position).getTitle());
+        holder.mProductLocation.setText(products.get(position).getLocation());
+        holder.mProductPhone.setText(products.get(position).getPhone());
+        holder.mDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,products.get(position).toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+        holder.mProductPhone.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_DIAL,
+                        Uri.parse("tel:" + products.get(position).getPhone()));
+                context.startActivity(intent);
+            }
+        });
+        switch (products.get(position).getImage()){
+            case 0:
+                holder.image.setImageResource(R.drawable.mac);
+                break;
+            case 1:
+                holder.image.setImageResource(R.drawable.alienware);
+                break;
+        }
+        Bitmap bitmap = ((BitmapDrawable)holder.mProductThumbnail.getDrawable()).getBitmap();
+        holder.mProductThumbnail.setImageBitmap(bitmap);
+    }
+
+    public int getItemCount(){
+        return products.size();
+    }
+}
